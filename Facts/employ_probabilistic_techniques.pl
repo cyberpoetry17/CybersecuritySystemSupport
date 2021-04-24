7aef2518cc0099ec17988be5a79f69e6d0529a74
nidza0 <43273670+nidza0@users.noreply.github.com>
2021-04-24 18:11:59 +0200

    Updated PL facts with attributes

--- Facts/employ_probabilistic_techniques.pl
+++ Facts/employ_probabilistic_techniques.pl
@@ -1,44 +1,65 @@
-%(attack,weakness,percentage)
+% weakness(attack, weakness, percentage).
+% attributes(attack_name, [id, likelihood of attack, typical severity]).
 
+%Fuzzing for application mapping
+attack(fuzzing_for_application_mapping).
+attributes(fuzzing_for_application_mapping, [215, high, low]).
 weakness(fuzzing_for_application_mapping,generation_of_error_message_containing_sensitive_information,1).
-weakness(fuzzing_for_application_mapping,7pk_-_errors,1).
+weakness(fuzzing_for_application_mapping,pk_errors,1).
 weakness(fuzzing_for_application_mapping,insertion_of_sensitive_information_into_log_file,1).
 
-weakness(dictionary-based_password_attack,weak_password_requirements,1).
-weakness(dictionary-based_password_attack,Not Using Password Aging,1).
-weakness(dictionary-based_password_attack,password_aging_with_long_expiration,1).
-weakness(dictionary-based_password_attack,reliance_on_a_single_factor_in_a_security_decision,1).
-weakness(dictionary-based_password_attack,improper_restriction_of_excessive_authentication_attempts,1).
-weakness(dictionary-based_password_attack,use_of_single-factor_authentication,1).
-weakness(dictionary-based_password_attack,use_of_password_system_for_primary_authentication,1).
 
+%Dictionary-based Password Attack
+attack(dictionary_based_password_attack).
+attributes(dictionary_based_password_attack, [16, medium, high]).
+weakness(dictionary_based_password_attack,weak_password_requirements,1).
+weakness(dictionary_based_password_attack,Not Using Password Aging,1).
+weakness(dictionary_based_password_attack,password_aging_with_long_expiration,1).
+weakness(dictionary_based_password_attack,reliance_on_a_single_factor_in_a_security_decision,1).
+weakness(dictionary_based_password_attack,improper_restriction_of_excessive_authentication_attempts,1).
+weakness(dictionary_based_password_attack,use_of_single_factor_authentication,1).
+weakness(dictionary_based_password_attack,use_of_password_system_for_primary_authentication,1).
+
+%Rainbow Table Password Cracking
+attack(rainbow_table_password_cracking,weak_encoding_for_password).
+attributes(rainbow_table_password_cracking,weak_encoding_for_password, [55, medium, medium]).
 weakness(rainbow_table_password_cracking,weak_encoding_for_password,1).
 weakness(rainbow_table_password_cracking,weak_password_requirements,1).
 weakness(rainbow_table_password_cracking,not_using_password_aging,1).
 weakness(rainbow_table_password_cracking,password_aging_with_long_expiration,1).
 weakness(rainbow_table_password_cracking,reliance_on_a_single_factor_in_a_security_decision,1).
-weakness(rainbow_table_password_cracking,owasp_top_ten_2007_category_a8_-_insecure_cryptographic_storage,1).
+weakness(rainbow_table_password_cracking,owasp_top_ten_2007_category_a8_insecure_cryptographic_storage,1).
 weakness(rainbow_table_password_cracking,use_of_password_hash_with_insufficient_computational_effort,1).
-weakness(rainbow_table_password_cracking,use_of_single-factor_authentication,1).
+weakness(rainbow_table_password_cracking,use_of_single_factor_authentication,1).
 weakness(rainbow_table_password_cracking,use_of_password_system_for_primary_authentication,1).
 
+%Password Spraying
+attack(password_spraying).
+attributes(password_spraying, [565, high, high]).
 weakness(password_spraying,weak_password_requirements,1).
 weakness(password_spraying,not_using_password_aging,1).
 weakness(password_spraying,password_aging_with_long_expiration,1).
 weakness(password_spraying,reliance_on_a_single_factor_in_a_security_decision,1).
 weakness(password_spraying,improper_restriction_of_excessive_authentication_attempts,1).
-weakness(password_spraying,use_of_single-factor_authentication,1).
+weakness(password_spraying,use_of_single_factor_authentication,1).
 weakness(password_spraying,use_of_password_system_for_primary_authentication,1).
 
+%Try Common or Default Usernames and Passwords
+attack(try_common_or_default_usernames_and_passwords).
+attributes(try_common_or_default_usernames_and_passwords, [70, medium, high]).
 weakness(try_common_or_default_usernames_and_passwords,weak_password_requirements,1).
 weakness(try_common_or_default_usernames_and_passwords,not_using_password_aging,1).
 weakness(try_common_or_default_usernames_and_passwords,password_aging_with_long_expiration,1).
-weakness(try_common_or_default_usernames_and_passwords,use_of_hard-coded_credentials,1).
+weakness(try_common_or_default_usernames_and_passwords,use_of_hard_coded_credentials,1).
 weakness(try_common_or_default_usernames_and_passwords,reliance_on_a_single_factor_in_a_security_decision,1).
-weakness(try_common_or_default_usernames_and_passwords,use_of_single-factor_authentication,1).
+weakness(try_common_or_default_usernames_and_passwords,use_of_single_factor_authentication,1).
 weakness(try_common_or_default_usernames_and_passwords,use_of_password_system_for_primary_authentication,1).
 
+
+%Encryption Brute Forcing
+attack(encryption_brute_forcing).
+attributes(encryption_brute_forcing, [20, low, low]).
 weakness(encryption_brute_forcing,inadequate_encryption_strength,1).
 weakness(encryption_brute_forcing,use_of_a_broken_or_risky_cryptographic_algorithm,1).
 weakness(encryption_brute_forcing,protection_mechanism_failure,1).
-weakness(encryption_brute_forcing,owasp_top_ten_2007_category_a8_-_insecure_cryptographic_storage,1).
+weakness(encryption_brute_forcing,owasp_top_ten_2007_category_a8_insecure_cryptographic_storage,1).
--- Facts/manipulate_timing_state.pl
+++ Facts/manipulate_timing_state.pl
@@ -1,21 +1,24 @@
-#(attack,weakness,percentage)
-#1
+% weakness(attack, weakness, percentage).
+% attributes(attack_name, [id, likelihood of attack, typical severity]).
+
+%Forced Deadlock
+attack(forced_deadlock).
+attributes(forced_deadlock, [25, low, high]).
 weakness(forced_deadlock,unrestricted_externally_accessible_lock,1).
 weakness(forced_deadlock,unsynchronized_access_to_shared_data_in_a_multithreaded_context,1).
 weakness(forced_deadlock,improper_locking,1). 
 weakness(forced_deadlock,deadlock,1).
 weakness(forced_deadlock,use_of_blocking_code_in_single_threaded,_non_blocking_context,1).
 
-
-
-#2
+%Leveraging Race Conditions via Symbolic Links
+attack(leveraging_race_condition_via_symbolic_links).
+attributes(leveraging_race_condition_via_symbolic_links, [27, medium, high]).
 weakness(leveraging_race_condition_via_symbolic_links,time_of_check_time_of_use_race_condition,1).
 weakness(leveraging_race_condition_via_symbolic_links,unix_symbolic_link_following,1).
 weakness(leveraging_race_condition_via_symbolic_links,improper_synchronization,1).
 weakness(leveraging_race_condition_via_symbolic_links,permission_race_condition_during_resource_copy,1).
 weakness(leveraging_race_condition_via_symbolic_links,improper_locking,1).
 
-
-
-#3
+%Bypassing of Intermediate Forms in Multiple-Form Sets
+attributes(bypassing_of_intermediate_forms_in_multiple_form_sets, [140, null, medium]).
 weakness(bypassing_of_intermediate_forms_in_multiple_form_sets,incomplete_internal_state_distinction,1).
