52007d22b4e1853bb912ffe54286707e7fdfcc99
RemaxX7 <marko.rakic98@gmail.com>
2021-05-31 12:20:19 +0100

    fix: updated csv file and rules

--- Rules/rules.pl
+++ Rules/rules.pl
@@ -5,3 +5,7 @@ retrieve_mitigations(Attack_name, Mitigations) :- attack(Attack_name), mitigatio
 retrieve_weaknesses(Attack_name,Weakness):-attack(Attack_name),weakness(Attack_name,W,P),append([],W,Weakness).
 
 retrieve_attributes(Attack_name,Attribute):-attack(Attack_name),attributes(Attack_name,A),append([],A,Attribute).
+
+retrieve_attributes_for_attack(C,X):- retrieve_mitigations(C,X);retrieve_weaknesses(C,X);retrieve_attributes(C,X).
+
+retrieve_measure_for_gathered(X1,X2,X3,M):-retrieve_mitigations(X1,M),retrieve_weaknesses(X2,M),retrieve_attributes(X3,M).
