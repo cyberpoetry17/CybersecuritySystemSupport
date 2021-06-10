64874ed98ee2b818a0befbcfff907188a28f9b76
nidza0 <43273670+nidza0@users.noreply.github.com>
2021-06-10 19:53:16 +0200

    feat: Initialize client app (frontend)

---
+++ client-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { RegisterAttackComponent } from './register-attack/register-attack.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalculatorComponent,
+    RegisterAttackComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
